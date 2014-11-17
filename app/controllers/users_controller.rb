6ca1aecd3910926982bfc743737c8c89a0e46bcc
Nathan Brown <nkabrown@gmail.com>
2014-11-17 15:46:26 -0500

    Add User sign-up and authentication
    * generate UserController
    * add user new/show views and actions
    * configure user routes
    * add form_for on user sign-up page
    * write authenticate method on UsersController

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to Prolific!"
+      redirect_to user_path(@user)
+    else
+      flash[:alert] = "We've encountered a problem. Please try again."
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+end
