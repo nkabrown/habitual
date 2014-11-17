868464352adfb81174e084d1436b9a1f6afe0f7e
Nathan Brown <nkabrown@gmail.com>
2014-11-17 16:15:36 -0500

    Add user log-in and log-out
    * add SessionsController
    * add sessions new view and sessions actions
    * add form_tag log-in to sessions#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   resources :users, only: [:new, :create, :show]
 
   # The priority is based upon order of creation: first created -> highest priority.
